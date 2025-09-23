2403a4e0069675cde6d5373360134570673836aa
Todor Trundev <trundev@gmail.com>
2025-09-23 08:24:25 +0300

    Fix user data-base racing condition, etc.
    Protect multithreaded access to g_user_database.
    Add delete_user() functionality for future development.

--- server_side/user_data.cpp
+++ server_side/user_data.cpp
@@ -1,11 +1,20 @@
 #include <string>
+#include <mutex>
 #include <map>
 #include "user_data.h"
 
 
+// Map of user-name to UserData and guard mutex
+//
+// Note:
+// user_database_mutex can be locked while clients_mutex is locked,
+// esp. in delete user scenario (dead-lock notice).
 std::map<std::string, UserData> g_user_database;
+std::mutex user_database_mutex;
 
 UserData *find_user(const std::string &name, bool do_create) {
+    // Guard access to database
+    std::lock_guard<std::mutex> lock(user_database_mutex);
     auto it = g_user_database.find(name);
     if (it != g_user_database.end()) {
         return &(it->second);
@@ -19,6 +28,18 @@ UserData *find_user(const std::string &name, bool do_create) {
     }
 
     return nullptr;
+}
+
+bool delete_user(const UserData &user) {
+    // Guard access to database
+    std::lock_guard<std::mutex> lock(user_database_mutex);
+    auto it = g_user_database.find(user.get_name());
+    if (it != g_user_database.end()) {
+        return false;
+    }
+
+    g_user_database.erase(it);
+    return true;
 }
 
 UserData::UserData() {
